d98cd3e4aadf30cc2de197d5ad9663de410c101d
eva <evacaruanac@gmail.com>
2026-04-22 23:36:01 +0200

    added home text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,16 @@ import picture from "../../assets/img/marvel-homee.jpg";
 const Home = () => {
   return (
     <main className="home-section">
-      <div>
+      <div className="home-hero">
         <img className="home-img" src={picture} alt="hero picture" />
+        <div className="home-overlay">
+          <div className="home-content">
+            <h1 className="home-title">MARVEL UNIVERSE</h1>
+            <p className="home-subtitle">
+              Explore les personnages et comics de l'univers Marvel
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
